fcb5d8dc08c704f05f02e7189254f3b0c71d1649
Julesaarts <122618860+Julesaarts@users.noreply.github.com>
2025-09-26 21:48:17 +0200

    Add project files.

---
+++ MoveIT/Models/EmployeeModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoveIT.Models
+{
+    public class EmployeeModel
+    {
+        public int Id { get; private set; }
+
+        //onderstaande is nodig om velden te connecten met de Page class
+        [Required]
+        [StringLength(maximumLength: 100, MinimumLength = 3)]
+        public string Name { get; private set; }
+    }
+}
