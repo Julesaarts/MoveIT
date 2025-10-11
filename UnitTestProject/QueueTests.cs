bccbde891834954f313674027973a1d56b5db7fe
Julesaarts <122618860+Julesaarts@users.noreply.github.com>
2025-10-11 14:44:35 +0200

    Added empty tests

---
+++ UnitTestProject/QueueTests.cs
@@ -0,0 +1,29 @@
+namespace UnitTestProject
+{
+    public class QueueTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            //terugkijken les 9-10
+            //beginnen met kijken op 26:00
+
+            // Arrange
+
+            // Act
+            //is altijd maar 1 regel code (1 test)
+
+            // Assert
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
